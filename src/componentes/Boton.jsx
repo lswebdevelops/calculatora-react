a7a0db15122993ea486f8e07670f992e32b500b5
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2023-04-09 14:36:25 -0300

    adding mathjs to app.js

--- src/componentes/Boton.jsx
+++ src/componentes/Boton.jsx
@@ -7,9 +7,9 @@ function Boton(props){
   };
     return(
       <div
-      className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}>
-
-        {props.children}
+      className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}
+      onClick={() => props.manejarClic(props.children)}>
+       {props.children}
       </div>
     )
 }
