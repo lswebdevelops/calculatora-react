9270e9856e9ff730c54b81bad3221e560c0b49a8
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2023-04-09 15:25:39 -0300

    Updates

---
+++ precache-manifest.2165003979471788d6762d61582f9064.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "24ddc105b66a1ee0999a",
+    "url": "/static/css/main.83818879.chunk.css"
+  },
+  {
+    "revision": "24ddc105b66a1ee0999a",
+    "url": "/static/js/main.24ddc105.chunk.js"
+  },
+  {
+    "revision": "34ad460ff6276f4d84a4",
+    "url": "/static/js/1.34ad460f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6ecec9cfef14bd70268c40f627c4773e",
+    "url": "/static/media/freecodecamp-logo.6ecec9cf.png"
+  },
+  {
+    "revision": "7590ba04d68dadb360b45a53676e80e4",
+    "url": "/index.html"
+  }
+];
